779c737594a78a2348601eaf0780939798cfbb57
Nebuleon Fumika <nebuleon.fumika@gmail.com>
2014-02-27 08:21:08 +0000

    Use the numerator of the scaling fraction as the number of coefficients when both downscaling and upscaling.

--- ipu/ipu.c
+++ ipu/ipu.c
@@ -206,7 +206,7 @@ static void ipu_set_resize_params(struct ipu *ipu,
 	if (srcW != dstW) {
 		/* Set the horizontal resize coefficients */
 		ipu_set_resize_coef(ipu, &fracW, REG_HRSZ_COEF_LUT);
-		coef_index = (((fracW.num >= fracW.denom) ? fracW.num : fracW.denom) - 1) << 16;
+		coef_index = (fracW.num - 1) << 16;
 
 		set_bit(ipu, REG_CTRL, IPU_CTRL_HRSZ_EN);
 	}
@@ -214,7 +214,7 @@ static void ipu_set_resize_params(struct ipu *ipu,
 	if (srcH != dstH) {
 		/* Set the vertical resize coefficients */
 		ipu_set_resize_coef(ipu, &fracH, REG_VRSZ_COEF_LUT);
-		coef_index |= ((fracH.num >= fracH.denom) ? fracH.num : fracH.denom) - 1;
+		coef_index |= fracH.num - 1;
 
 		set_bit(ipu, REG_CTRL, IPU_CTRL_VRSZ_EN);
 	}
